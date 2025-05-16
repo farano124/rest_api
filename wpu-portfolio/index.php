6213ac5d2f81f8e46936a3c3d3baf5c37ee10e4b
farano124 <fthrano168500@gmail.com>
2025-05-16 19:47:31 +0700

    Membuat jumlah subscriber & follower serta upload terakhir di yt & ig

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -78,10 +78,50 @@
         </div>
 
         <div class="row justify-content-center">
-          <div class="col-md-5"></div>
-            <h5>WebProgrammingUNPAS</h5>
-            <img src="img/profile1.png" width="100">
-          <div class="col-md-5"></div>
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col md-4">
+                <img src="img/profile1.png" width="100" class="rounded-circle-img-thumbnail">
+              </div>
+              <div class="col-md-8">
+                <h5>WebProgrammingUNPAS</h5>
+                <p>70000 Subscriber.</p>
+              </div>
+            </div>
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-BmTKA1xCm8?rel=0" allowfullscreen></iframe>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col md-4">
+                <img src="img/profile1.png" width="100" class="rounded-circle-img-thumbnail">
+              </div>
+              <div class="col-md-8">
+                <h5>SandhikaGalih</h5>
+                <p>70000 Followers.</p>
+              </div>
+            </div>
+
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/1.png">
+                </div>
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/2.png">
+                </div>
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/3.png">
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
      </section>
