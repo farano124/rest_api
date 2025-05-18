66cd75b08b7156ee823b32abbb301ebb306aed94
farano124 <fthrano168500@gmail.com>
2025-05-18 17:27:34 +0700

    Menambahkan model untuk post dan put pada API

--- wpu-rest-server/application/models/Mahasiswa_model.php
+++ wpu-rest-server/application/models/Mahasiswa_model.php
@@ -13,4 +13,16 @@ class Mahasiswa_model extends CI_Model{
         $this->db->delete('mahasiswa', ['id' => $id]);
         return $this->db->affected_rows();
     }
+
+    public function createMahasiswa($data){
+        $this->db->insert('mahasiswa', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function updateMahasiswa($data, $id){
+        $this->db->update('mahasiswa', $data, ['id' => $id]);
+        return $this->db->affected_rows();
+
+    }
+   
 }
